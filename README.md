ab7073bfcb4c1e21af5cb12463a7c0c6bb04a134
ethanG <ethangunzb@gmail.com>
2024-09-30 23:11:41 +0200

    setup

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # langage_en_C
+https://nospy.notion.site/Projet-Cr-ation-d-un-Interpr-teur-de-Langage-de-Programmation-e890d1624945429eafc4ce91cbcce315
---
+++ cmake-build-debug/CMakeFiles/lanage_en_C.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lanage_en_C.dir/main.c.obj"
+  "CMakeFiles/lanage_en_C.dir/main.c.obj.d"
+  "lanage_en_C.exe"
+  "lanage_en_C.exe.manifest"
+  "lanage_en_C.pdb"
+  "liblanage_en_C.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/lanage_en_C.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
